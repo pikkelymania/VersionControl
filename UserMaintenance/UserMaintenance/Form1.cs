01c15548446da98b8ce1460f9ffcdac00aa260bf
david <david@LAPTOP-30EKSJB5>
2026-04-19 18:37:54 +0200

    Törlés funkció megvalósítása

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -25,6 +25,7 @@ namespace UserMaintenance
             label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
             button2.Text = Resource1.Save;
+            button3.Text = Resource1.Delete;
 
             // ListBox összekötése a listával
             listBox1.DataSource = users;
@@ -57,6 +58,14 @@ namespace UserMaintenance
                         sw.WriteLine($"{user.ID};{user.FullName}");
                     }
                 }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                users.Remove((User)listBox1.SelectedItem);
             }
         }
     }
