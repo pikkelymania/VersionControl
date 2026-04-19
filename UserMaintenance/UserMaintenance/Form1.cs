7600e979bb62555fe40fc72b764081794d2e225b
david <david@LAPTOP-30EKSJB5>
2026-04-19 18:14:26 +0200

    Refaktorálás: Név mezők összevonása FullName tulajdonságba és UI frissítése

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -21,8 +21,7 @@ namespace UserMaintenance
             InitializeComponent();
 
             // Feliratok beállítása a Resource fájlból
-            label1.Text = Resource1.LastName;
-            label2.Text = Resource1.FirstName;
+            label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
 
             // ListBox összekötése a listával
@@ -35,8 +34,7 @@ namespace UserMaintenance
         {
             var u = new User()
             {
-                LastName = textBox1.Text,
-                FirstName = textBox2.Text
+                FullName = textBox1.Text,
             };
             users.Add(u);
         }
