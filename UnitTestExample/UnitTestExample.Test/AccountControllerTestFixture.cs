c07a4982489464962d55f1008cf21926f9d7ee38
david <david@LAPTOP-30EKSJB5>
2026-04-19 21:46:06 +0200

    E-mail validálás tesztek megírva

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UnitTestExample.Controllers; // Behozzuk az eredeti projekt Controllerét
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [TestCase("abcd1234", false)]
+        [TestCase("irf@uni-corvinus", false)]
+        [TestCase("irf.uni-corvinus.hu", false)]
+        [TestCase("irf@uni-corvinus.hu", true)]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            var accountController = new AccountController();
+
+            var actualResult = accountController.ValidateEmail(email);
+
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+    }
+}
