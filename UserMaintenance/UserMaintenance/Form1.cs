9ccd52bd4e0e53bb965c2f85c8e4108e11cd99cb
david <david@LAPTOP-30EKSJB5>
2026-04-19 18:06:11 +0200

    Hozzáadás funkció és erőforrások bekötése

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
+        // A lista, ami az adatokat tárolja
+        BindingList<User> users = new BindingList<User>();
+
         public Form1()
         {
             InitializeComponent();
+
+            // Feliratok beállítása a Resource fájlból
+            label1.Text = Resource1.LastName;
+            label2.Text = Resource1.FirstName;
+            button1.Text = Resource1.Add;
+
+            // ListBox összekötése a listával
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }
