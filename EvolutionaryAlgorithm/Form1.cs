dd0b9f0e02af389b0fcf72d0fd471c374717e8a1
david <david@LAPTOP-30EKSJB5>
2026-04-20 14:02:51 +0200

    Játéktér megjelenítése és induló populáció (100 gép) létrehozása

--- EvolutionaryAlgorithm/Form1.cs
+++ EvolutionaryAlgorithm/Form1.cs
@@ -7,14 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace EvolutionaryAlgorithm
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+
+            gc.Start();
         }
     }
 }
