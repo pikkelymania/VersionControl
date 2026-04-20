755ed05b18270857b9356704c3dc356baaa30c30
david <david@LAPTOP-30EKSJB5>
2026-04-20 14:09:10 +0200

    Evolúciós iteráció és új generáció létrehozása Címke hozzáadása a felülethez

--- EvolutionaryAlgorithm/Form1.cs
+++ EvolutionaryAlgorithm/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -28,9 +29,43 @@ namespace EvolutionaryAlgorithm
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
+            gc.GameOver += Gc_GameOver;
+
             for (int i = 0; i < populationSize; i++)
             {
                 gc.AddPlayer(nbrOfSteps);
+            }
+
+            gc.Start();
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format("{0}. generáció", generation);
+            label1.BringToFront();
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            gc.ResetCurrentLevel();
+
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
             }
 
             gc.Start();
